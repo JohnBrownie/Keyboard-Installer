321521430a33154ca1350b90e67ead3c1f88530e
John Brownie <john_brownie@sil.org>
2017-11-09 14:23:08 +1000

    Initial Commit

---
+++ Keyboard Installer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Keyboard Installer
+//
+//  Created by John Brownie on 9/11/17.
+//  Copyright © 2017 John Brownie. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ Keyboard Installer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Keyboard Installer
+//
+//  Created by John Brownie on 9/11/17.
+//  Copyright © 2017 John Brownie. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
